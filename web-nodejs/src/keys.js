4d6faa7cb355e8f29ec3f3d5d06895c7e1bbe2ad
María Fernanda Elizalde Macías <85259595+mfem442@users.noreply.github.com>
2023-04-25 10:26:13 -0600

    Delete web-nodejs directory

--- web-nodejs/src/keys.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    database: {
-        host: process.env.MYSQL_HOST || "localhost",
-        user: process.env.MYSQL_USER || "root",
-        password: process.env.MYSQL_PASSWORD || "Mp4135*6",
-        database: process.env.MYSQL_DATABASE || "itesm502"
-    }
-}
--- web-nodejs/src/lib/handlebars.js
+++
@@ -1,8 +0,0 @@
-const moment = require('moment')
-
-const helpers = {}
-
-moment.locale('es'); 
-
-
-module.exports = helpers;
