dfa6798932a9c8ad3b906fa678738202530f0671
Mafer <A01634135@tec.mx>
2023-04-24 19:16:13 -0600

    Creacion de sesiones

--- rick-morty-express-handlebars-main/src/database.js
+++ rick-morty-express-handlebars-main/src/database.js
@@ -4,10 +4,22 @@ const {database} = require('./keys');
 
 const pool = mysql.createPool(database);
 
-//
-//
-//
-
+pool.getConnection((err, connection) => {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.error('DATABASE CONNECTION WAS CLOSED', err);
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR'){
+            console.error('DATABASE HAS TOO MANY CONNECTIONS', err);
+        }
+        if (err.code === 'ECONNREFUSED'){
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+        if (connection) connection.release();
+        console.log('DB IS CONNECTED');
+        return;
+    }
+})
 // Create promises from callbacks
 pool.query = promisify(pool.query);
 
--- rick-morty-express-handlebars-main/src/index.js
+++ rick-morty-express-handlebars-main/src/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const path = require('path');
-
+const session = require('express-session')
 
 const { database } = require('./keys')
 
@@ -17,7 +17,22 @@ app.set('port', process.env.PORT || 4000);
 // Public
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Sessions
+app.use(flash());
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+app.use(passport.initialized());
+app.use(passport.session());
+
+app.use(session({
+    secret: 'itesm502',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore(database)
+}));
+
 // Starting the server
 app.listen(app.get('port'), () => {
     console.log("Server on port: ", app.get('port'))
-})
+});
