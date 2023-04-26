0496d43cd3f531d34326b7442dad29b15aaa0ba0
Abraham González <65511679+abrahamglez17@users.noreply.github.com>
2023-04-26 13:18:23 -0600

    passport signup

--- web-nodejs/src/lib/passport.js
+++ web-nodejs/src/lib/passport.js
@@ -24,6 +24,23 @@ passport.use('local.signin', new LocalStrategy({
 
 }))
 
+passport.use('local.signup', new LocalStrategy({
+    usernameField: 'username',
+    passwordField: 'password',
+    passReqToCallback: true
+}, async(req, username, password, done) => {
+    const { fullname } = req.body;
+    const newUser = {
+        username,
+        password,
+        fullname
+    }
+    newUser.password = await encryptPassword(password);
+    const result = await pool.query('INSERT INTO User SET ?', [newUser]);
+    newUser.id = result.insertId;
+    return done(null, newUser);
+}))
+
 passport.serializeUser((user, done) => {
     done(null, user.id);
 });
