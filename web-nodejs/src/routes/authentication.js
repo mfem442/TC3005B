a33d4c2c1fb06e1cb5589c40fd80ecba4bd5fa88
Abraham González <65511679+abrahamglez17@users.noreply.github.com>
2023-04-26 13:03:30 -0600

    establecimiento de rutas y view de signup

--- web-nodejs/src/routes/authentication.js
+++ web-nodejs/src/routes/authentication.js
@@ -13,10 +13,21 @@ router.get('/signin', isNotLoggedIn, (req, res) => {
 router.post('/signin', isNotLoggedIn, (req, res, next) => {
     passport.authenticate('local.signin', {
         successRedirect: '/profile',
-        failureRedirect: '/signin',
+        failureRedirect: '/',
         failureFlash: true
     })(req, res, next)
 })
+
+router.get('/signup', isNotLoggedIn, (req, res) => {
+    res.render('auth/signup')
+})
+
+router.post('/signup', isNotLoggedIn, passport.authenticate('local.signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}))
+
 
 router.get('/profile', isLoggedIn, (req, res) => {
     res.render('profile');
